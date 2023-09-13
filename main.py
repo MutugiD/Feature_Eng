a28ca660a4562831b15231d58236862f53cc8871
MutugiD <dennismutugi@hotmail.com>
2023-09-14 01:24:26 +0300

    data ingestion experiment

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from mlFeature import logger 
+from mlFeature.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom ML project!!!")
+logger.info("Welcome to custome ML pipeline")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ test.py
@@ -0,0 +1,3 @@
+from mlFeature.constants import *
+from mlFeature.utils.common import read_yaml, create_directories
+from mlFeature.entity.config_entity import DataIngestionConfig
