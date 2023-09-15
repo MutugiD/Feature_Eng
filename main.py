e557ba12561b1cfd649fa7e959440a3c84545c2f
MutugiD <dennismutugi@hotmail.com>
2023-09-15 14:04:43 +0300

    ingestion + validation

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from mlFeature import logger 
 from mlFeature.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlFeature.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 logger.info("Welcome to custome ML pipeline")
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
